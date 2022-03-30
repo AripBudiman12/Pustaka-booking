0d5209e888566ee0d560cfe0c1415988b02aa07e
AripBudiman12 <arip24.ab@gmail.com>
2022-03-30 11:05:40 +0700

    -Membuat file views/v_about -Menambahkan function about pada Controllers/Web

---
+++ application/views/v_about.php
@@ -0,0 +1,19 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Arip</li>
+        <li>Nama Belakang : Budiman</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Bambu Larangan No.65 Jakartta Barat</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
